2e836a3229d2a662221c45f35fbc13664392ba99
thruuuuuu <167851340+thruuuuuu@users.noreply.github.com>
2026-03-14 22:16:21 +0530

    Applying coordinates of the test transformer

--- Geospatial Mapping & Transformer Suitability Model/test_suitability.py
+++ Geospatial Mapping & Transformer Suitability Model/test_suitability.py
@@ -2,3 +2,12 @@ import requests
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix, classification_report
+
+API = 'http://localhost:5000'
+
+payload = {
+    'latitude'     : 6.849,
+    'longitude'    : 79.9247,
+    'solarCapacity': 5.0,
+    'searchRadius' : 5000
+}
