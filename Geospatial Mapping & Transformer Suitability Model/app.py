ae75a6cca74814e548df14cc8bf95c01e84b6514
thruuuuuu <167851340+thruuuuuu@users.noreply.github.com>
2026-03-16 15:13:10 +0530

    imports added for streamlit

---
+++ Geospatial Mapping & Transformer Suitability Model/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import math
+import folium
+from streamlit_folium import st_folium
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.cluster import KMeans
+from sklearn.linear_model import LinearRegression
+from geopy.distance import geodesic
